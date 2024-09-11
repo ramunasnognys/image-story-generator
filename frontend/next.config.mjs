c2ff62e1c99ec0ad4b87b99bcada496dff76b14b
Ramunas <dev.nognys@gmail.com>
2024-09-11 10:46:44 +0300

    Update Vercel and Next.js configurations

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    BACKEND_URL: process.env.BACKEND_URL,
-  },
-};
-
-export default nextConfig;
+    env: {
+      BACKEND_URL: process.env.BACKEND_URL,
+    },
+    output: 'standalone',
+  };
+  
+  export default nextConfig;
